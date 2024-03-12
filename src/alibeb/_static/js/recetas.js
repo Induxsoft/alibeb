d4b03028b5e875da97ef6c4e4bf13873c29c0eb8
FabianAlvarez1 <93471911+FabianAlvarez1@users.noreply.github.com>
2024-03-11 18:17:04 -0600

    Avances: aplicación de recetas

--- src/alibeb/_static/js/recetas.js
+++ src/alibeb/_static/js/recetas.js
@@ -5,13 +5,17 @@ var recetas =
     table_rec_vari: null,
     inputk_product: null,
     replace_rect_c: true,
+    replace_rect_v: true,
     minrowsRecConst: 10,
+    minrowsRecVarbl: 10,
 
     init()
     {
         this.table_rec_cons = document.querySelector('#table_receta_constante');
+        this.table_rec_vari = document.querySelector('#table_receta_variable');
         this.inputk_product = document.querySelector('#ik_producto');
-
+        this.inputk_product_reemplazo = document.querySelector('#ik_producto_reeplazo');
+        this.inputk_grupos = document.querySelector('#ik_grupos');
 
         const check_flagu = document.querySelector('#check_flagutil');
         const check_flagl = document.querySelector('#check_flaglimites');
@@ -28,24 +32,25 @@ var recetas =
         const tipocomision = document.querySelector('select[name="tipocomision"]');
         const ipt_utilmin = document.querySelector('input[name="utilmin"]');
         const ipt_costoultimo = document.querySelector('input[name="costoultimo"]');
+        const check_ingadic = document.querySelector('#check_ingadic');
 
-        console.log(this.inputk_product);
+        ['ipt_costomanoobra','ipt_costoindirecto'].forEach(id=>{
+            const input = document.getElementById(id);
+            if (input) input.addEventListener('keyup', () => this.setCosts());
+        });
 
-        if (this.inputk_product && this.table_rec_cons)
-        {
-            this.table_rec_cons.setInputKey("codigo",this.inputk_product);
-            this.table_rec_cons.setInputKey("descripcion",this.inputk_product);
-            this.inputk_product.addEventListener('change', producto => this.agregarProductoRecetaConstante(producto));
-        }
+        this.setTableEvents();
+        this.setTableConfigs();
 
         if (check_flagu && ipt_flagutl) check_flagu.addEventListener('change', e => { this.set_ipt_check_value(ipt_flagutl, check_flagu) });
         if (check_flagl && ipt_flaglim) check_flagl.addEventListener('change', e => { this.set_ipt_check_value(ipt_flaglim, check_flagl) });
         if (check_viewp && ipt_visible) check_viewp.addEventListener('change', e => { this.set_ipt_check_value(ipt_visible, check_viewp) });
 
-        if (form_cntnr) form_cntnr.addEventListener('submit', e => { this.create_update_product(e) });
+        if (form_cntnr) form_cntnr.addEventListener('submit', e => this.create_update_product(e));
         if (formacomision) formacomision.addEventListener('change', e => this.change_comision_field(formacomision));
         if (tipocomision) tipocomision.addEventListener('change', e => this.select_comision_type(tipocomision));
         if (ipt_utilmin && ipt_costoultimo) ipt_utilmin.addEventListener('keyup', e => this.calcule_utilidad(ipt_utilmin, ipt_costoultimo));
+        if (check_ingadic) check_ingadic.addEventListener('change', () => this.disableAdicional(!check_ingadic.checked) );
 
         if (ipt_unidad && ipt_factorb && ipt_unidadb && msg_factor)
         {
@@ -53,6 +58,45 @@ var recetas =
             ipt_unidadb.addEventListener('keyup', e => { this.valide_factor(ipt_unidad,ipt_factorb,ipt_unidadb,msg_factor) });
             ipt_factorb.addEventListener('change', e => { this.valide_factor(ipt_unidad,ipt_factorb,ipt_unidadb,msg_factor) });
             ipt_factorb.addEventListener('keyup', e => { this.valide_factor(ipt_unidad,ipt_factorb,ipt_unidadb,msg_factor) });
+        }
+    },
+
+    // =============== TABLES EVENTS AND CONFIGS
+
+    setTableEvents()
+    {
+        if (this.table_rec_cons)
+        {
+            const T = this.table_rec_cons;
+            const E = T.EdiTable.Const.Events;
+            T.Events[E.FieldUpdated] = (e) => { this.fieldUpdatedRecCon(e); }
+        }
+        if (this.table_rec_vari)
+        {
+            const T = this.table_rec_vari;
+            const E = T.EdiTable.Const.Events;
+            T.Events[E.FieldUpdated] = (e) => { this.fieldUpdatedRecVar(e); }
+            T.Events[E.BeforeUpdateCell] = (e) => { this.BeforeUpdateCellRecVar(e); }
+        }
+    },
+    setTableConfigs()
+    {
+        if (this.inputk_product && this.table_rec_cons)
+        {
+            this.table_rec_cons.onTdPaint = (td,idx,indexcol,field) => this.setCellColors(td,indexcol,this.table_rec_cons);
+            this.table_rec_cons.setInputKey("codigo",this.inputk_product);
+            this.table_rec_cons.setInputKey("descripcion",this.inputk_product);
+            this.table_rec_cons.setInputKey("reemplazo",this.inputk_product_reemplazo);
+            this.inputk_product.addEventListener('change', producto => this.addProdRecCon(producto));
+            this.inputk_product_reemplazo.addEventListener('change', producto => this.addProdReemRecCon(producto));
+            this.table_rec_cons._printRows();
+        }
+        if (this.inputk_grupos && this.table_rec_vari)
+        {
+            this.table_rec_vari.onTdPaint = (td,idx,indexcol,field) => this.setCellColors(td,indexcol,this.table_rec_vari);
+            this.table_rec_vari.setInputKey("grupo_desc",this.inputk_grupos);
+            this.inputk_grupos.addEventListener('change', grupo => this.addGrupoRecVar(grupo));
+            this.table_rec_vari._printRows();
         }
     },
 
@@ -115,14 +159,35 @@ var recetas =
 
         msg.textContent = `1 ${unit2.value} es equivalente a ${factor.value} ${unit1.value}`;
     },
+    setCellColors(td,indexcol,table)
+    {
+        let coldef = table.Columns[indexcol];
+        if (coldef && coldef.type.toLowerCase() == 'noeditable')
+        {
+            td.style.backgroundColor = '#FAFAFA';
+            td.style.color = '#000';
+        }
+    },
+    autoFillTable(table,rows)
+    {
+        let length = table.DataArray.length;
+        if (length < rows) {
+            for (let i = length; i < rows; i++) table.DataArray.push({});
+        }
+    },
+    getTableList(table,valideField)
+    {
+        return table.DataArray.filter(data => data[valideField]!=undefined);
+    },
 
     // =============== RECETA CONSTANTE
-    searchProdRecetaConst()
+
+    searchProdRecCon()
     {
         this.replace_rect_c = false;
-        if (this.inputk_product) this.inputk_product.searchText('',true);
+        if (this.inputk_product) this.inputk_product.searchText('%',true);
     },
-    agregarProductoRecetaConstante(producto)
+    addProdRecCon(producto)
     {
         if (!producto) return;
 
@@ -151,37 +216,169 @@ var recetas =
         }
         else
         {
-            let list = this.getRecetaConstanteList();
+            let list = this.getTableList(this.table_rec_cons, 'elemento');
             list.push(dataRow);
             this.table_rec_cons.DataArray = list;
-            this.autoFillTableRecetaConstante();
         }
         
         this.replace_rect_c = true;
-        this.refreshRepresentacion();
+        this.autoFillTable(this.table_rec_cons,this.minrowsRecConst);
+        this.table_rec_cons._printRows();
+        this.calcsRecCon();
     },
-    getRecetaConstanteList()
+    addProdReemRecCon(producto)
     {
-        return this.table_rec_cons.DataArray.filter(data => data.elemento!=undefined);
-    },
-    refreshRepresentacion()
-    {
-        const cantidad_total = this.table_rec_cons.DataArray.reduce((total,obj) => total+Number(obj.cantidad??0), 0);
-        this.table_rec_cons.DataArray.forEach((producto,index)=>{
-            producto['representacion'] = Math.mul(Math.div((producto.cantidad??0),cantidad_total),100);
-            if (producto['representacion'] == '0') producto['representacion'] = '';
-            this.table_rec_cons.UpdateRow(index);
-        });
-    },
-    autoFillTableRecetaConstante()
-    {
-        let length = this.table_rec_cons.DataArray.length;
-        if (length < this.minrowsRecConst)
+        if (!producto) return;
+
+        let idx = this.table_rec_cons.CurrentRowIndex();
+        if (idx == -1) return;
+
+        let data = this.table_rec_cons.DataArray[idx];
+        if (data)
         {
-            for (let i = length; i < length; i++) {
-                this.table_rec_cons.DataArray.push({});
+            data['reemplazo'] = producto.descripcion;
+            data['reemplazo_pk'] = producto.sys_pk;
+            this.table_rec_cons.UpdateRow(idx);
+        }
+    },
+    calcsRecCon()
+    {
+        const cantidad_total = this.table_rec_cons.DataArray.reduce((total,obj) => Math.add(total,Number(obj.cantidad??0)), 0);
+        let importe_total = 0;
+        this.table_rec_cons.DataArray.forEach((producto,index)=>{
+            if (producto.elemento != undefined) {
+                producto['representacion'] = Math.mul(Math.div(Number(producto.cantidad??0),cantidad_total),100);
+                producto['importe'] = Math.mul(Number(producto.cantidad??0),Number(producto.costoultimo??0));
+                importe_total += Number(producto.importe);
+                this.table_rec_cons.UpdateRow(index);
+            }
+        });
+        const RC_cdirecto = document.querySelector('#RC_cdirecto');
+        if (RC_cdirecto) RC_cdirecto.value = importe_total;
+        this.setCosts();
+    },
+    fieldUpdatedRecCon(e)
+    {
+        if (e.field == 'cantidad')
+        {
+            this.calcsRecCon();
+        }
+    },
+    deleteProdRecCon()
+    {
+        this.table_rec_cons.DeleteCurrentRow();
+    },
+
+    // =============== RECETA VARIABLE
+
+    searchProdRecVar()
+    {
+        this.replace_rect_v = false;
+        if (this.inputk_grupos) this.inputk_grupos.searchText('%',true);
+    },
+    addGrupoRecVar(grupo)
+    {
+        if (!grupo) return;
+
+        let dataRow = {
+            sys_pk: 0,
+            sys_recver: 0,
+            grupo: grupo.grupo,
+            grupo_desc: grupo.grupo_desc,
+            descripcion: '',
+            factor: 1,
+            maximo: 1,
+            minimo: 1,
+            padicional: '0 - Ninguno',
+            cdirectoprom: grupo.cdirectoprom,
+            cmanoobraprom: grupo.cmanoobraprom,
+            cindirectoprom: grupo.cindirectoprom,
+            _cdirectoprom: grupo._cdirectoprom,
+            _cmanoobraprom: grupo._cmanoobraprom,
+            _cindirectoprom: grupo._cindirectoprom
+        }
+
+        if (this.replace_rect_v)
+        {
+            let idx = this.table_rec_vari.CurrentRowIndex();
+            if (!this.table_rec_vari.DataArray[idx]) this.table_rec_vari.DataArray[idx] = {};
+            this.table_rec_vari.DataArray[idx] = dataRow;
+        }
+        else
+        {
+            let list = this.getTableList(this.table_rec_vari, 'grupo');
+            list.push(dataRow);
+            this.table_rec_vari.DataArray = list;
+        }
+        
+        this.replace_rect_v = true;
+        this.autoFillTable(this.table_rec_vari,this.minrowsRecVarbl);
+        this.table_rec_vari._printRows();
+        this.calcsRecVar();
+    },
+    calcsRecVar()
+    {
+        let importe_total = 0;
+        this.table_rec_vari.DataArray.forEach((grupo,index)=>{
+            if (grupo.grupo != undefined) {
+                const fac = Number(grupo.factor??1);
+                const max = Number(grupo.maximo??1);
+                const mul = Math.mul(fac, (max>0?max:1));
+                grupo['cdirectoprom'] = Math.mul(mul,Number(grupo._cdirectoprom??0));
+                grupo['cmanoobraprom'] = Math.mul(mul,Number(grupo._cmanoobraprom??0));
+                grupo['cindirectoprom'] = Math.mul(mul,Number(grupo._cindirectoprom??0));
+                this.table_rec_vari.UpdateRow(index);
+
+                importe_total += grupo.cdirectoprom;
+            }
+        });
+        const RV_costo = document.querySelector('#RV_costo');
+        if (RV_costo) RV_costo.value = importe_total;
+        this.setCosts();
+    },
+    fieldUpdatedRecVar(e)
+    {
+        if (e.field == 'factor' || e.field == 'maximo')
+        {
+            this.calcsRecVar();
+        }
+    },
+    BeforeUpdateCellRecVar(e)
+    {
+        const T = this.table_rec_vari;
+        let data = T.DataArray[T.RowIndexOfTd(e.td)];
+        if (!data) return;
+
+        switch(e.coldef.field)
+        {
+            case 'factor': { if (Number(e.text) < 1) { alert('Factor incorrecto'); e.cancel = true; } break; }
+            case 'maximo': { if (Number(e.text) < Number(data.minimo??0)){ alert('El valor máximo debe ser mayor o igual al valor mínimo'); e.cancel = true } break; }
+            case 'minimo': { 
+                if (Number(e.text) < 0) { alert('El valor mínimo debe ser mayor o igual a cero'); e.cancel = true; }
+                if (Number(e.text) > Number(data.maximo??0)) { alert('El valor mínimo debe ser menor o igual que el máximo'); e.cancel = true; }
+                break;
             }
         }
+    },
+    deleteProdRecVar()
+    {
+        this.table_rec_vari.DeleteCurrentRow();
+    },
+
+    // =============== PRODUCCIÓN Y COSTOS
+
+    setCosts()
+    {
+        const ipt_rc_total = document.querySelector('#RC_cdirecto');
+        const ipt_rv_total = document.querySelector('#RV_costo');
+        const ipt_cdirecto = document.querySelector('#ipt_costodirecto');
+        const ipt_costotal = document.querySelector('#ipt_costototprod');
+        const ipt_manoobra = document.querySelector('#ipt_costomanoobra');
+        const ipt_indirect = document.querySelector('#ipt_costoindirecto');
+        const costo_ultimo = document.querySelector('input[name="costoultimo"]');
+        ipt_cdirecto.value = Math.add(Number(ipt_rc_total.value), Number(ipt_rv_total.value));
+        ipt_costotal.value = Math.add(Number(ipt_cdirecto.value), Number(ipt_manoobra.value), Number(ipt_indirect.value));
+        costo_ultimo.value = ipt_costotal.value;
     },
 
     // =============== PRECIOS
@@ -263,6 +460,11 @@ var recetas =
         ipt_prices.forEach(ipt =>{
             ipt.value = Math.add(precio, percent);
         });
+    },
+    disableAdicional(disable=true)
+    {
+        const inputs = document.querySelectorAll('.ipt-adicional');
+        inputs.forEach(input=>input.toggleAttribute('disabled',disable));
     }
 }
 
